55acb7fd8bae50250f8c7e1b1b3f4cb645bb1f33
Dimka <dima@wearepush.co>
2019-06-01 18:39:47 +0300

    refactored tweets index

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,12 +1,8 @@
 class TweetsController < ApplicationController
-  
   def index
     response = HTTParty.get('http://twitter.com/newrelic')
-    
     parsed_data = Nokogiri::HTML.parse response.body
-    tweetNodes = parsed_data.css(".js-tweet-text")
-    @nodes = tweetNodes.collect do |node|
-      node.inner_html
-    end
+    tweet_nodes = parsed_data.css('.js-tweet-text')
+    @nodes = tweet_nodes.collect(&:inner_html)
   end
 end
