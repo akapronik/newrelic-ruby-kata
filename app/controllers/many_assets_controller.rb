c1d43e6060b1ba8ebe813205981c8c66c8e321b6
Dimka <dima@wearepush.co>
2019-06-02 10:24:08 +0300

    fixed many assets

--- app/controllers/many_assets_controller.rb
+++ app/controllers/many_assets_controller.rb
@@ -4,11 +4,11 @@ class ManyAssetsController < ApplicationController
   def index
     @num_wide = 20
     @num_high = 20
-    @icons = Icon.limit(@num_wide * @num_high).includes(:web_site)
+    @icons = Icon.includes(web_site: :icon).limit(@num_wide * @num_high)
   end
 
-  def display
-    icon = Icon.find(params[:id])
-    render :text => icon.icon, :content_type => "image/x-icon", :layout => false
-  end
+  # def display
+  #   icon = Icon.find(params[:id])
+  #   render :text => icon.icon, :content_type => "image/x-icon", :layout => false
+  # end
 end
