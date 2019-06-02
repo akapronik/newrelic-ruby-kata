8384d2989cb710cdb48ed3889d9447dfe0babb73
Dimka <dima@wearepush.co>
2019-06-02 10:28:10 +0300

    fixed errors

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,13 +1,7 @@
 class ErrorsController < ApplicationController
-  def index
-  end
-  
+  def index; end
+
   def ajax
-    value = some_internal_logic(params['p'].to_i)
-    render :text => value
-  end
-  
-  def some_internal_logic(x)
-    return 1 / x
+    render :text => params['p'].to_i
   end
 end
